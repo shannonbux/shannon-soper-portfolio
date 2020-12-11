dcdb376d7e1e66e3e900371fd42761d462a6c7ad
shannonbux <shannon.soper@gmail.com>
2020-12-11 14:07:17 -0800

    favicon updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,7 +20,7 @@ module.exports = {
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: `standalone`,
-        icon: `src/pages/images/zen-circle-1.png`, // This path is relative to the root of the site.
+        icon: `src/pages/images/icons/hello.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-image`,
